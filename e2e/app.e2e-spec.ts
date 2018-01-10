e8fe26be26b6ecea4f904d45d2a44d734c358d31
Surajit Rana <surajit.cool@gmail.com>
2018-01-10 09:10:30 +0530

    Created boilerplate for Lazy loading in Angular

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('ng-lazy-load-app App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
